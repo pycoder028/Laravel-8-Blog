c1af77148f15d6bf15c046d217a5f509956bd569
Forhad-Hossain <pycoder028@gmail.com>
2024-08-24 00:12:16 +0600

    Add Category with Form Request & fetch data using eloquent

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.master')
+
+@section('title','Category')
+
+@section('content')
+
+<div class="container-fluid px-4">
+    
+    <div class="card mt-4">
+        <div class="card-header">
+            <h4>View Category
+                <a href="{{ url('admin/add-category') }}" class="btn btn-primary btn-sm float-end">Add Category</a>
+            </h4>
+        </div>
+        <div class="card-body">
+            @if(session('message'))
+                <div class="alert alert-success">{{ session('message') }}</div>
+            @endif
+
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Category Name</th>
+                        <th>Image</th>
+                        <th>Status</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($category as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>
+                                <img src="{{ asset('uploads/category/'.$item->image) }}" width="50px" height="50px" alt="Image">
+                            </td>
+                            <td>{{ $item->status == '1' ? 'Hidden':'Shown' }}</td>
+                            <td>
+                                <a href="" class="btn btn-success">Edit</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+</div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
+/* Admin routes group */
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
+
     Route::get('/dashboard',[\App\Http\Controllers\Admin\DashboardController::class,'index']);
+    Route::get('/category',[\App\Http\Controllers\Admin\CategoryController::class,'index']);
+    Route::get('/add-category',[\App\Http\Controllers\Admin\CategoryController::class,'create']);
+    Route::post('/add-category',[\App\Http\Controllers\Admin\CategoryController::class,'store']);
+
 });
