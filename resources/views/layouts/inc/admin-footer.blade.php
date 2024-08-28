62ec5437d1c9444d82659f505f75542395c00962
Riyad <pycoder028@gmail.com>
2024-08-29 00:46:27 +0600

    Set Current Navbar and Sidebar nav link active

--- resources/views/layouts/inc/admin-footer.blade.php
+++ resources/views/layouts/inc/admin-footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2024</div>
+            <div class="text-muted">Copyright &copy; Developed by <a href="https://www.facebook.com/pycoder028/">dev@Forhad</a></div>
             <div>
                 <a href="#">Privacy Policy</a>
                 &middot;
